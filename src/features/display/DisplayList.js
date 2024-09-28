83188b667bc8e5d22066f355ebf0862574df7d0b
Mann Jaiswal <mannjaiswal5@gmail.com>
2024-09-27 18:22:41 -0700

    Added animation to DisplayList.js using react spring library

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,8 +1,8 @@
 import { Col, Row} from 'reactstrap';
-import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
+import  AnimatedDisplayCard  from './AnimatedDisplayCard';
 
 const DisplayList = () => {
     const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
@@ -11,10 +11,12 @@ const DisplayList = () => {
         <Row>
             {items.map((item, idx)=> {
                 return (
+                    item && (
                     <Col md className='m1' key={idx}>
-                        <DisplayCard item={item}/>
+                        <AnimatedDisplayCard item={item}/>
                     </Col>
-                );
+                )
+            );
             })}
         </Row>
     );
