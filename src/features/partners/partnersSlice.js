11ff3386d5c3bcd54b3d326842ee8194965820fd
Mann Jaiswal <mannjaiswal5@gmail.com>
2024-10-12 19:57:34 -0700

    Modified displayList.js to add error handiling when fetching featured items from db.json server file

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -49,5 +49,10 @@ return state.partners.partnersArray;
 };
 
 export const selectFeaturedPartner = (state) => {
-    return state.partners.partnersArray.find((partner)=> partner.featured);
+    return{
+         featuredItem: state.partners.partnersArray.find(
+            (partner)=> partner.featured),
+            isLoading: state.partners.isLoading,
+            errMsg: state.partners.errMsg
+    };
 };
